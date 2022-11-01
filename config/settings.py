25fe2a4ff4b69514c585ad9ed89467d0a5ea0270
nishesheshe <radmir.ahunov@yandex.ru>
2022-11-01 17:46:35 +0300

    REST_FRAMEWORK authentication by token set

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.BookStoreUser'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication', # new
+    ],
+
+}
