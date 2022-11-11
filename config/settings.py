20d89980bc65b8b8a0bd9e6433f6ecc7497b288f
nishesheshe <radmir.ahunov@yandex.ru>
2022-11-11 13:05:45 +0300

    authentication is not required anymore to use api

--- config/settings.py
+++ config/settings.py
@@ -142,9 +142,6 @@ AUTH_USER_MODEL = 'users.BookStoreUser'
 SITE_ID = 1
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
